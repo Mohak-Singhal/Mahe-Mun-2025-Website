84d8b868da0aaa09234f0b765f6dcc38e180a52f
Twinkle <tomartwinkle2572gmail.com>
2025-03-16 16:47:57 +0530

    updated team-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Team from "./pages/team";
 
 
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
     </Router>
   );
