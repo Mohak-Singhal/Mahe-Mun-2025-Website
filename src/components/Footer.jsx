3c1948c9417d22223ca1d0c8cf901b7138185f0c
Krishna Anand <krishnanand86654@gmail.com>
2025-08-15 01:07:20 +0530

    trial social link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -124,10 +124,10 @@ const Footer = () => {
             <div className="flex flex-col items-center md:items-start">
               <h3 className="text-xl font-semibold mb-4">Follow Us</h3>
               <div className="flex space-x-4 text-gray-400">
-              <a href="https://www.linkedin.com/in/oiac-mahe-93b338254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="text-xl hover:text-white transition-colors duration-200">
+              <a href="https://www.linkedin.com/in/oiac-mahe-93b338254/" className="text-xl hover:text-white transition-colors duration-200">
                   <FaLinkedin />
                 </a>
-              <a href="https://www.instagram.com/oiac_mahe?igsh=MXdqYTZ2aTZpNjdzZg==" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-white transition-colors duration-200">
+              <a href="https://www.instagram.com/oiac_mahe/" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-white transition-colors duration-200">
                   <FaInstagram />
                 </a>
                 <a href="#" className="text-xl hover:text-white transition-colors duration-200">
